78ca6f80f1af83354906d6daee091a6d1ac8740c
Rohit Padwal <rpadwal98@gmail.com>
2026-05-02 16:51:40 -0500

    intial checkin

---
+++ src/main/java/com/example/orderservice/controller/OrderController.java
@@ -0,0 +1,33 @@
+package com.example.orderservice.controller;
+
+import com.example.orderservice.model.User;
+import com.example.orderservice.service.UserService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Arrays;
+import java.util.List;
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    private final WebClient webClient;
+
+    @Value("${app.services.user-service.base-url}")
+    private String userServiceUrl;
+
+    public OrderController() {
+        this.webClient = WebClient.builder().build();
+    }
+
+    @GetMapping("/user/{id}")
+    public String getUserFromOrderService(@PathVariable Long id) {
+
+        return webClient.get()
+                .uri(userServiceUrl + "/users")
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+    }
+}
